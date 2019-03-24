a0ae4ff75f1fdff3ae8a13d59b6764c41d13210f
Chasen Sherman <csherman16@berkeley.edu>
2019-03-24 08:27:49 -0700

    changes for steam js

--- app/static/steam.js
+++ app/static/steam.js
@@ -1,26 +1,28 @@
-function update_game_styling(){
+function update_game_styling() {
     for (var table = 1; table <= 2; table++) { // update for changed & steam tables
         var table_string = "game_table" + table;
-        var games = document.getElementById(table_string).getElementsByTagName("tbody")[0].getElementsByClassName('clickable');
-        for (index = 0; index < games.length; index++) {
-            var game_data = $(games[index]).attr('data-game')
-            $.ajax({
-                type: "POST",
-                url: "/steam_update",
-                data: { game: game_data},
-                ajaxI: index, // Capture the correct value of index.
-                success: function(result){
-                    var info_to_update = JSON.parse(result)
-                    var tds = games[this.ajaxI].getElementsByTagName("td")
-                    if (info_to_update.change_vector[0] || info_to_update.change_vector[1] || info_to_update.steam_vector[0]) {
-                        $(tds[1])[0].style.backgroundColor = "green";
-                    } else if (info_to_update.change_vector[2] || info_to_update.change_vector[3] || info_to_update.steam_vector[1]) {
-                        $(tds[2])[0].style.backgroundColor = "green";
-                    } else if (info_to_update.change_vector[4] || info_to_update.change_vector[5] || info_to_update.steam_vector[2]) {
-                        $(tds[3])[0].style.backgroundColor = "green";
+        if (document.getElementById(table_string) { // both tables may not be there
+            var games = document.getElementById(table_string).getElementsByTagName("tbody")[0].getElementsByClassName('clickable');
+            for (index = 0; index < games.length; index++) {
+                var game_data = $(games[index]).attr('data-game')
+                $.ajax({
+                    type: "POST",
+                    url: "/steam_update",
+                    data: { game: game_data},
+                    ajaxI: index, // Capture the correct value of index.
+                    success: function(result){
+                        var info_to_update = JSON.parse(result)
+                        var tds = games[this.ajaxI].getElementsByTagName("td")
+                        if (info_to_update.change_vector[0] || info_to_update.change_vector[1] || info_to_update.steam_vector[0]) {
+                            $(tds[1])[0].style.backgroundColor = "green";
+                        } else if (info_to_update.change_vector[2] || info_to_update.change_vector[3] || info_to_update.steam_vector[1]) {
+                            $(tds[2])[0].style.backgroundColor = "green";
+                        } else if (info_to_update.change_vector[4] || info_to_update.change_vector[5] || info_to_update.steam_vector[2]) {
+                            $(tds[3])[0].style.backgroundColor = "green";
+                        }
                     }
-                }
-            })
+                })
+            }
         }
     }
     setTimeout(update_game_styling, 150000); //150000
